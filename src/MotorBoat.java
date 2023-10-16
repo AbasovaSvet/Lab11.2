7bceda4328623091c10b00e18c862fe522c3c18a
Svetlana <A.svet.mr@yandex.ru>
2023-10-16 13:26:03 +0300

    v1

--- src/MotorBoat.java
+++ src/MotorBoat.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MotorBoat {
+public class MotorBoat {
+    boolean on_engine ;
+    boolean off_engine;
+
+
+    public void statusEngine(){
+        on_engine = true;
+    }
+    public void start_engine(){
+        on_engine = false;
+        if (on_engine = false){
+            off_engine = true;
+            System.out.printf("звуки тарахтения");
+        }
+    }
 }
